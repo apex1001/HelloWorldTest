dbe60a8b3440da221e493015630561773072dbf3
apex1001 <apex1001@home.nl>
2012-09-17 19:40:35 +0200

    ggggg
    Signed-off-by: apex1001 <apex1001@home.nl>

--- HelloWorld/src/HelloWorld.java
+++ HelloWorld/src/HelloWorld.java
@@ -7,7 +7,7 @@ public class HelloWorld {
 	public static void main(String[] args)
 	{
 		System.out.println("Hello World");
-		System.out.println("Today we go to college");
+		System.out.println("Today we go to college2");
 	}
 
 }
